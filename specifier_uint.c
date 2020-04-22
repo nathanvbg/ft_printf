8d047023d7661b573a4ca311c7c5402a2c4e5ada
nathan verbruggen <nathanvbg@MBP-de-nathan.lan>
2020-04-23 01:17:09 +0200

    specifier x et X termine + debut de p

--- specifier_uint.c
+++ specifier_uint.c
@@ -19,3 +19,48 @@ void	specifier_uint(va_list arg, int *p, int *i, t_flag *flag)
 	len = ft_strlen(x);
 	tri_flags(p, i, flag, x, len);
 }
+
+void	specifier_x(va_list arg, int *p, int *i, t_flag *flag)
+{
+	char			*x;
+	unsigned int	nb;//unsigned int?
+	int				len;
+
+	nb = va_arg(arg, unsigned int);
+	if (!(x = ft_itoa_base(nb, "0123456789abcdef", 16)))
+		ft_free(&x);
+	len = ft_strlen(x);
+	tri_flags(p, i, flag, x, len);
+}
+
+void	specifier_hex(va_list arg, int *p, int *i, t_flag *flag)
+{
+	char			*x;
+	unsigned int	nb;//unsigned int?
+	int				len;
+
+	nb = va_arg(arg, unsigned int);
+	if (!(x = ft_itoa_base(nb, "0123456789ABCDEF", 16)))
+		ft_free(&x);
+	len = ft_strlen(x);
+	tri_flags(p, i, flag, x, len);
+}
+
+void	specifier_pointer(va_list arg, int *p, int *i, t_flag *flag)
+{
+	char	*x;
+	size_t	nb;//unsigned int?
+	int		len;
+
+	nb = va_arg(arg, size_t);
+	if (!(x = ft_itoa_base(nb, "0123456789abcdef", 18)))
+		ft_free(&x);
+	len = ft_strlen(x);
+	//if ((flag->n1 > len) && (flag->n1 > flag->n2)) 
+	//else
+	//{
+		ft_putchar('0', p);
+		ft_putchar('x', p);
+		tri_flags(p, i, flag, x, len);
+	//}
+}
